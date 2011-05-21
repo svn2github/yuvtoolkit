91437c4d957b72e40950c45d93c77f1decad684e
yuheng.zhao@gmail.com <yuheng.zhao@gmail.com@fe7d2ec1-bd83-e687-9167-72ef5a783f6b>
2011-05-21 20:13:36 +0000

    added script to automatically make a build (only tested on windows so far)
    git-svn-id: http://yuvtoolkit.googlecode.com/svn@32 fe7d2ec1-bd83-e687-9167-72ef5a783f6b

---
+++ trunk/Setup/new_build.py
@@ -0,0 +1,20 @@
+import os, platform
+
+if platform.system() == "Windows":
+    # Increase build number only in windows
+    # so run this scrip in windows first, commit and 
+    # run scrip in other OSs
+    import increment_build_number
+    
+    os.environ["QTDIR"] = "C:\\QT\\4.7.3"
+    devenv = os.environ["ProgramFiles"] + "\\Microsoft Visual Studio 8\\Common7\\IDE\\devenv.exe"
+    nsis = os.environ["ProgramFiles"] + "\\NSIS\\makensis.exe"
+
+    os.system("\""+devenv+"\" ..\\YUVToolkitProject_vs2005.sln /Clean Release /out clean_log.txt")
+    os.system("\""+devenv+"\" ..\\YUVToolkitProject_vs2005.sln /Rebuild Release /out build_log.txt")
+    os.system("\""+nsis+"\" YUVToolkit.nsi")
+elif platform.system() == "Darwin":
+    os.chdir("..")
+    os.system("qmake YUVToolkitProject.pro")
+    os.system("make clean")
+    os.system("make")
